446f5a39cfb7570114d4c8ccc75e1e259272d700
daffaalfikri <149288996+daffaalfikri@users.noreply.github.com>
2023-12-22 22:47:33 +0700

    Add files via upload

---
+++ Data.py
@@ -0,0 +1,253 @@
+"""
+Description     : Simple Python implementation of the Apriori Algorithm
+
+Usage:
+    $python apriori.py -f DATASET.csv -s minSupport  -c minConfidence
+
+    $python apriori.py -f DATASET.csv -s 0.15 -c 0.6
+"""
+
+from apriori import runApriori, dataFromFile, to_str_results
+import pandas as pd
+import sys
+
+from itertools import chain, combinations
+from collections import defaultdict
+from optparse import OptionParser
+
+
+def subsets(arr):
+    """ Returns non empty subsets of arr"""
+    return chain(*[combinations(arr, i + 1) for i, a in enumerate(arr)])
+
+
+def returnItemsWithMinSupport(itemSet, transactionList, minSupport, freqSet):
+    """calculates the support for items in the itemSet and returns a subset
+    of the itemSet each of whose elements satisfies the minimum support"""
+    _itemSet = set()
+    localSet = defaultdict(int)
+
+    for item in itemSet:
+        for transaction in transactionList:
+            if item.issubset(transaction):
+                freqSet[item] += 1
+                localSet[item] += 1
+
+    for item, count in localSet.items():
+        support = float(count) / len(transactionList)
+
+        if support >= minSupport:
+            _itemSet.add(item)
+
+    return _itemSet
+
+
+def joinSet(itemSet, length):
+    """Join a set with itself and returns the n-element itemsets"""
+    return set(
+        [i.union(j)
+         for i in itemSet for j in itemSet if len(i.union(j)) == length]
+    )
+
+
+def getItemSetTransactionList(data_iterator):
+    transactionList = list()
+    itemSet = set()
+    for record in data_iterator:
+        transaction = frozenset(record)
+        transactionList.append(transaction)
+        for item in transaction:
+            itemSet.add(frozenset([item]))  # Generate 1-itemSets
+    return itemSet, transactionList
+
+
+def runApriori(data_iter, minSupport, minConfidence):
+    """
+    run the apriori algorithm. data_iter is a record iterator
+    Return both:
+     - items (tuple, support)
+     - rules ((pretuple, posttuple), confidence)
+    """
+    itemSet, transactionList = getItemSetTransactionList(data_iter)
+
+    freqSet = defaultdict(int)
+    largeSet = dict()
+    # Global dictionary which stores (key=n-itemSets,value=support)
+    # which satisfy minSupport
+
+    assocRules = dict()
+    # Dictionary which stores Association Rules
+
+    oneCSet = returnItemsWithMinSupport(
+        itemSet, transactionList, minSupport, freqSet)
+
+    currentLSet = oneCSet
+    k = 2
+    while currentLSet != set([]):
+        largeSet[k - 1] = currentLSet
+        currentLSet = joinSet(currentLSet, k)
+        currentCSet = returnItemsWithMinSupport(
+            currentLSet, transactionList, minSupport, freqSet
+        )
+        currentLSet = currentCSet
+        k = k + 1
+
+    def getSupport(item):
+        """local function which Returns the support of an item"""
+        return float(freqSet[item]) / len(transactionList)
+
+    toRetItems = []
+    for key, value in largeSet.items():
+        toRetItems.extend([(tuple(item), getSupport(item)) for item in value])
+
+    toRetRules = []
+    for key, value in list(largeSet.items())[1:]:
+        for item in value:
+            _subsets = map(frozenset, [x for x in subsets(item)])
+            for element in _subsets:
+                remain = item.difference(element)
+                if len(remain) > 0:
+                    confidence = getSupport(item) / getSupport(element)
+                    if confidence >= minConfidence:
+                        toRetRules.append(
+                            ((tuple(element), tuple(remain)), confidence))
+    return toRetItems, toRetRules
+
+
+def printResults(items, rules):
+    """prints the generated itemsets sorted by support and the confidence rules sorted by confidence"""
+    for item, support in sorted(items, key=lambda x: x[1]):
+        print("item: %s , %.3f" % (str(item), support))
+    print("\n------------------------ RULES:")
+    for rule, confidence in sorted(rules, key=lambda x: x[1]):
+        pre, post = rule
+        print("Rule: %s ==> %s , %.3f" % (str(pre), str(post), confidence))
+
+
+def to_str_results(items, rules):
+    """prints the generated itemsets sorted by support and the confidence rules sorted by confidence"""
+    i, r = [], []
+    for item, support in sorted(items, key=lambda x: x[1]):
+        x = "item: %s , %.3f" % (str(item), support)
+        i.append(x)
+
+    for rule, confidence in sorted(rules, key=lambda x: x[1]):
+        pre, post = rule
+        x = "Rule: %s ==> %s , %.3f" % (str(pre), str(post), confidence)
+        r.append(x)
+
+    return i, r
+
+
+def dataFromFile(fname):
+    """Function which reads from the file and yields a generator"""
+    with open(fname, "rU") as file_iter:
+        for line in file_iter:
+            line = line.strip().rstrip(",")  # Remove trailing comma
+            record = frozenset(line.split(","))
+            yield record
+
+
+if __name__ == "__main__":
+
+    optparser = OptionParser()
+    optparser.add_option(
+        "-f", "--inputFile", dest="input", help="filename containing csv", default=None
+    )
+    optparser.add_option(
+        "-s",
+        "--minSupport",
+        dest="minS",
+        help="minimum support value",
+        default=0.15,
+        type="float",
+    )
+    optparser.add_option(
+        "-c",
+        "--minConfidence",
+        dest="minC",
+        help="minimum confidence value",
+        default=0.6,
+        type="float",
+    )
+
+    (options, args) = optparser.parse_args()
+
+    inFile = None
+    if options.input is None:
+        inFile = sys.stdin
+    elif options.input is not None:
+        inFile = dataFromFile(options.input)
+    else:
+        print("No dataset filename specified, system with exit\n")
+        sys.exit("System will exit")
+
+    minSupport = options.minS
+    minConfidence = options.minC
+
+    items, rules = runApriori(inFile, minSupport, minConfidence)
+
+    printResults(items, rules)
+
+    import streamlit as st
+
+st.markdown("# Apriori Streamlit")
+
+st.sidebar.markdown(
+    """The code attempts to implement the following paper:
+
+> *Agrawal, Rakesh, and Ramakrishnan Srikant. "Fast algorithms for mining association rules." Proc. 20th int. conf. very large data bases, VLDB. Vol. 1215. 1994.*
+"""
+)
+
+default_csv = st.selectbox(
+    "Select one of the sample csv files", (
+        "Groceries data.csv", "Groceries data.csv")
+)
+
+if default_csv == 'INTEGRATED-DATASET.csv':
+    st.markdown('''The dataset is a copy of the “Online directory of certified businesses with a detailed profile” file from the 
+    Small Business Services (SBS) dataset in the NYC Open Data Sets http://nycopendata.socrata.com/''')
+elif default_csv == 'tesco.csv':
+    st.markdown(
+        'The dataset is a toy dataset contain frequently purchased grocery items')
+
+st.markdown('Here are some sample rows from the dataset')
+csv_file = pd.read_csv(default_csv, header=None, lineterminator="\n")
+st.write(csv_file[0].str.split("\,", expand=True).head())
+
+st.markdown('---')
+st.markdown("## Inputs")
+
+st.markdown('''
+            **Support** shows transactions with items purchased together in a single transaction.
+            
+            **Confidence** shows transactions where the items are purchased one after the other.''')
+
+st.markdown(
+    'Support and Confidence for Itemsets A and B can be represented by formulas')
+
+support_helper = ''' > Support(A) = (Number of transactions in which A appears)/(Total Number of Transactions') '''
+confidence_helper = ''' > Confidence(A->B) = Support(AUB)/Support(A)') '''
+st.markdown('---')
+
+support = st.slider("Enter the Minimum Support Value", min_value=0.1,
+                    max_value=0.9, value=0.15,
+                    help=support_helper)
+
+confidence = st.slider("Enter the Minimum Confidence Value", min_value=0.1,
+                       max_value=0.9, value=0.6, help=confidence_helper)
+
+inFile = dataFromFile(default_csv)
+
+items, rules = runApriori(inFile, support, confidence)
+
+i, r = to_str_results(items, rules)
+
+st.markdown("## Results")
+
+st.markdown("### Frequent Itemsets")
+st.write(i)
+
+st.markdown("### Frequent Rules")
+st.write(r)
